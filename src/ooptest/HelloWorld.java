ce557b19639a2faea65f2191aa919eb603479f11
namita098 <mind_roverden35@hotmail.com>
2017-11-21 15:19:52 +0700

    Testing add Data

--- src/ooptest/HelloWorld.java
+++ src/ooptest/HelloWorld.java
@@ -5,7 +5,11 @@ public class HelloWorld {
 
 
         System.out.println("Hello word, I'm MIND");
-
+//         Add Data
+        System.out.println("Namita Jaroenkhun");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management");
+        System.out.println("RMUTSV");
     }//main
 
 
