b8d393d03a1dcf378f1eddcf1dc9c31b9205d687
namita098 <mind_roverden35@hotmail.com>
2017-12-06 11:11:41 +0700

    TestNetedLoop

--- src/ooplab3/TestNetedLoop.java
+++ src/ooplab3/TestNetedLoop.java
@@ -2,12 +2,14 @@ package ooplab3;
 
 public class TestNetedLoop {
     public static void main(String[] args) {
-
-        for (int i=1;i<=5;i++){
+    int n=1;
+        for (int i=1;i<=8;i++){
             //statement
-            for (int j=1;j<=5;j++){
+            for (int j=1;j<=8;j++){
                 //statement
-                System.out.println("*");
+                if (i==8 ||j==i||j==1){
+                    System.out.print("*\t");
+                }else System.out.print(n+"\t");
             }//j
             System.out.println();
         }//i
