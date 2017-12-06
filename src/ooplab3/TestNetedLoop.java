84c33f8e7a7aa59b2a63e0650074a5c98d6db5c9
namita098 <mind_roverden35@hotmail.com>
2017-12-06 10:09:41 +0700

    TestDoWhileLoop

---
+++ src/ooplab3/TestNetedLoop.java
@@ -0,0 +1,17 @@
+package ooplab3;
+
+public class TestNetedLoop {
+    public static void main(String[] args) {
+
+        for (int i=1;i<=5;i++){
+            //statement
+            for (int j=1;j<=5;j++){
+                //statement
+                System.out.println("*");
+            }//j
+            System.out.println();
+        }//i
+
+
+    }//main
+}//class
