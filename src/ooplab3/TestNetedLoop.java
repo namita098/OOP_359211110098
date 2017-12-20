c4e02306ecf4f8b96818a7105a9483f64339156c
namita098 <mind_roverden35@hotmail.com>
2017-12-20 08:34:34 +0700

    sortingArray

--- src/ooplab3/TestNetedLoop.java
+++ src/ooplab3/TestNetedLoop.java
@@ -2,7 +2,7 @@ package ooplab3;
 
 public class TestNetedLoop {
     public static void main(String[] args) {
-    int n=1;
+        int n=1;
         for (int i=1;i<=8;i++){
             //statement
             for (int j=1;j<=8;j++){
