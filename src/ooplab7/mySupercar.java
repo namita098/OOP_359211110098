0bd6fb5f90f42e30fb433c6ca4c4fbf218cf1f03
namita098 <mind_roverden35@hotmail.com>
2018-01-28 20:46:48 +0700

    mySuperCar

--- src/ooplab7/mySupercar.java
+++ src/ooplab7/mySupercar.java
@@ -3,36 +3,36 @@ package ooplab7;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class mySupercar {
+
+public class mySuperCar {
     public static void main(String[] args) {
         ArrayList<Supercar> myCarList = new ArrayList<Supercar>();
 
         myCarList = inputData(myCarList);
         System.out.println("== Show Super Car Info ==");
-        for (int i=0;i<myCarList.size();i++) {
+        for (int i=0;i<myCarList.size();i++){
             System.out.println(myCarList.get(i).getSuperCarInfo());
         }
-
     }//main
 
     private static ArrayList inputData(ArrayList myCarList) {
-
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Insert Supercar info: ");
+        System.out.println("Insert your Supercar info: ");
         for (int i = 0; i < 10; i++) {
-            System.out.println("Super car: " + (i + 1));
+            System.out.println("Super car: "+(i+1));
             System.out.print("Brand: ");
-            String b = scanner.nextLine();
+            String Carbrand =scanner.nextLine();
             System.out.print("Color: ");
-            String c = scanner.nextLine();
+            String Carcolor = scanner.nextLine();
             System.out.print("Engine Size: ");
-            String e = scanner.nextLine();
+            String EngineSize = scanner.nextLine();
             System.out.print("Max Speed: ");
-            String m = scanner.nextLine();
-            Supercar car = new Supercar(b,c,e,m);
+            String Maxspeed = scanner.nextLine();
+            System.out.print("Country of origin: ");
+            String Countryoforigin = scanner.nextLine();
+            Supercar car  = new Supercar(Carbrand,Carcolor,EngineSize,Maxspeed,Countryoforigin);
             myCarList.add(car);
         }
         return myCarList;
-    }
-
+    }//inputData
 }//class
