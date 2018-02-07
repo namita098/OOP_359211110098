4364e6bb82c45ebb0dbade67836b79ecac6e8a89
namita098 <mind_roverden35@hotmail.com>
2018-02-07 11:01:24 +0700

    myCarApp.java

--- src/ooplap9/myCarApp.java
+++ src/ooplap9/myCarApp.java
@@ -2,13 +2,13 @@ package ooplap9;
 
 public class myCarApp {
     public static void main(String[] args) {
-        personID classPersen = new personID("1111111111111",
+        Address classPersen = new Address("359211110098",
                 "Puriwat Lertkrai",
                 new Address("109 M.2", "Nakorn",
                         "80110"),
-                new job("Teacher", 20000));
+                new job("Student",15000));
 
-        System.out.println(person.toString());
+       System.out.println(Address.class);
 
 
 
